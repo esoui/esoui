8facef81815985da663a79b607288aa823abbcc2
Arthur Corenzan <arthur@corenzan.com>
2016-06-15 16:53:36 -0300

    2.4.7

--- update.sh
+++ update.sh
@@ -3,8 +3,6 @@
 set -ue
 
 git checkout master
-rm -rf esoui
-cp -r "$1" .
 find esoui/ -type f -name "*.dds" > Textures.txt
 find esoui/ -type f -not -name "*.txt" -not -name "*.xml" -not -name "*.lua" -delete
 vi README.md
